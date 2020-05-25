caa3e570ab084a75a33c3f51e38a6c4870c91196
Ashim Sedhain <asimsedhain33@gmail.com>
2020-05-25 04:52:50 -0500

    Added POST tests and comments

--- app/test/test.js
+++ app/test/test.js
@@ -3,50 +3,51 @@ const expect = require('chai').expect;
 const request = require("supertest");
 const redis = require("redis");
 const MongoClient = require('mongodb').MongoClient;
-const fs = require("fs");
-const FormData = require("form-data");
+const path = require("path")
 
 
 describe("API Tests", () => {
 
+	// Env Varibales
 	const COLLECTION = process.env.collection || "images"
 	const DBURI = process.env.DBURI || "mongodb+srv://tester:L3u1kKTEYb3Z9Omh@images-ulhix.mongodb.net/test?retryWrites=true&w=majority";
 	const DBNAME = process.env.dbname || "Color"
 	const LISTNAME = process.env.list_name || "processing"
+
+
 	let dbClient;
 	let redisPublisher;
 
 
 
-	before("Setting up", (done) => {
+	before("Setting up", async () => {
+
+		// Passing the env varibles to app
 		app.set("COLLECTION", COLLECTION)
 		app.set("LISTNAME", LISTNAME)
 
+		// Creates a redis client
+		// Redis server does not need to be there
+		// All the events are queued until server is connected
+		// No server is needed for the tests to work
 		redisPublisher = redis.createClient();
-		console.log("Connected to redis");
-
 		app.set("redis", redisPublisher)
-		MongoClient.connect(DBURI, function (err, client) {
-			if (err) {
-				console.log(err)
-			}
-			console.log(`${new Date().toLocaleString()} Connected successfully to server`);
-			dbClient = client;
-			const db = client.db(DBNAME);
-			app.set("db", db)
-			done()
-		});
+
+		// Connecting to the database
+		// Database needs to be connected to get the data
+		const client = await MongoClient.connect(DBURI);
+		const db = client.db(DBNAME);
+		app.set("db", db)
+		dbClient = client
 	})
 
-	
 
 
-	let imageIds = ["5ec14ad608db08b724a2b4e0", "5ec14be608db082acaa2b4e1", "5ec14c1608db080a3ca2b4e3"];
-
+	// Testing all the GET endpoints
 	describe("# Get", () => {
 
 		// Testing all the sample image ids
-		for (let id of imageIds) {
+		for (let id of ["5ec14ad608db08b724a2b4e0", "5ec14be608db082acaa2b4e1", "5ec14c1608db080a3ca2b4e3"]) {
 			for (let type of ["original", "color"]) {
 				it(`should get ${type} sample image with id: ${id}`, async () => {
 					const res = await request(app).get(`/upload/${type}?id=${id}`)
@@ -57,33 +58,37 @@ describe("API Tests", () => {
 			}
 		}
 
+		// Testing invalid id
 		it("should return object with image field and null value", async () => {
 			const res = await request(app).get("/upload/color?id=asdfaf");
 			expect(res.statusCode).to.equal(404)
 			expect(res.headers["content-type"]).to.eql("application/json; charset=utf-8")
 			expect(res.body).to.be.a("object")
+			expect(res.body).to.have.property("Image")
 			expect(res.body.Image).to.be.null;
 		})
 
 
 	})
 
-	// describe("# Post", () => {
-	// 	it("should upload the image and return a imageId", async () => {
+	// Testing all the post endpoints
+	describe("# Post", () => {
 
-	// 		image = fs.readFileSync(".\\build\\sample_image_0.jpg")
-	// 		// console.log(image)
-	// 		const data = new FormData()
-	// 		data.append('Original', image)
-	// 		const res = await request(app).post("/upload").send(JSON.stringify(data));
-	// 		// expect(res.statusCode).to.equal(200);
-	// 		// expect(res.headers["content-type"]).to.eql("application/json; charset=utf-8")
-	// 		console.log(res.body)
-	// 	})
-	// })
+		// Testing image uploads
+		for (let imagePath of ["./build/sample_image_0.jpg", "../screen_shot.jpg", "../system_architecture.jpg"]) {
+			it("should upload the image and return a imageId", async () => {
+				const res = await request(app).post("/upload").attach("Original", path.resolve(imagePath))
+				expect(res.statusCode).to.equal(200);
+				expect(res.headers["content-type"]).to.eql("application/json; charset=utf-8")
+				expect(res.body).to.have.property("imageId")
+				expect(res.body.imageId).to.be.a("string")
+			})
+		}
+		
+	})
 
 
-	after("finisheing", () => {
+	after("Closing Connections", () => {
 		dbClient.close();
 		redisPublisher.quit();
 	})
