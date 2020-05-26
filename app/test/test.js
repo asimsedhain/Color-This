e1b1b9761960c59a1d3f5c5541cc89943bf27b09
Ashim Sedhain <asimsedhain33@gmail.com>
2020-05-25 21:08:08 -0500

    removed db uri and removed semi collons

--- app/test/test.js
+++ app/test/test.js
@@ -1,8 +1,8 @@
 const app = require("../app")
-const expect = require('chai').expect;
-const request = require("supertest");
-const redis = require("redis");
-const MongoClient = require('mongodb').MongoClient;
+const expect = require('chai').expect
+const request = require("supertest")
+const redis = require("redis")
+const MongoClient = require('mongodb').MongoClient
 const path = require("path")
 
 
@@ -10,13 +10,13 @@ describe("API Tests", () => {
 
 	// Env Varibales
 	const COLLECTION = process.env.collection || "images"
-	const DBURI = process.env.DBURI || "mongodb+srv://tester:L3u1kKTEYb3Z9Omh@images-ulhix.mongodb.net/test?retryWrites=true&w=majority";
+	const DBURI = process.env.DBURI
 	const DBNAME = process.env.dbname || "Color"
 	const LISTNAME = process.env.list_name || "processing"
 
 
-	let dbClient;
-	let redisPublisher;
+	let dbClient
+	let redisPublisher
 
 
 
@@ -30,13 +30,13 @@ describe("API Tests", () => {
 		// Redis server does not need to be there
 		// All the events are queued until server is connected
 		// No server is needed for the tests to work
-		redisPublisher = redis.createClient();
+		redisPublisher = redis.createClient()
 		app.set("redis", redisPublisher)
 
 		// Connecting to the database
 		// Database needs to be connected to get the data
-		const client = await MongoClient.connect(DBURI);
-		const db = client.db(DBNAME);
+		const client = await MongoClient.connect(DBURI)
+		const db = client.db(DBNAME)
 		app.set("db", db)
 		dbClient = client
 	})
@@ -51,21 +51,21 @@ describe("API Tests", () => {
 			for (let type of ["original", "color"]) {
 				it(`should get ${type} sample image with id: ${id}`, async () => {
 					const res = await request(app).get(`/upload/${type}?id=${id}`)
-					expect(res.statusCode).to.equal(200);
+					expect(res.statusCode).to.equal(200)
 					expect(res.headers["content-type"]).to.eql("image/jpeg")
-					expect(res.body).to.be.an("Uint8Array");
+					expect(res.body).to.be.an("Uint8Array")
 				})
 			}
 		}
 
 		// Testing invalid id
 		it("should return object with image field and null value", async () => {
-			const res = await request(app).get("/upload/color?id=asdfaf");
+			const res = await request(app).get("/upload/color?id=asdfaf")
 			expect(res.statusCode).to.equal(404)
 			expect(res.headers["content-type"]).to.eql("application/json; charset=utf-8")
 			expect(res.body).to.be.a("object")
 			expect(res.body).to.have.property("Image")
-			expect(res.body.Image).to.be.null;
+			expect(res.body.Image).to.be.null
 		})
 
 
@@ -78,7 +78,7 @@ describe("API Tests", () => {
 		for (let imagePath of ["./build/sample_image_0.jpg", "../screen_shot.jpg", "../system_architecture.jpg"]) {
 			it("should upload the image and return a imageId", async () => {
 				const res = await request(app).post("/upload").attach("Original", path.resolve(imagePath))
-				expect(res.statusCode).to.equal(200);
+				expect(res.statusCode).to.equal(200)
 				expect(res.headers["content-type"]).to.eql("application/json; charset=utf-8")
 				expect(res.body).to.have.property("imageId")
 				expect(res.body.imageId).to.be.a("string")
@@ -89,8 +89,8 @@ describe("API Tests", () => {
 
 
 	after("Closing Connections", () => {
-		dbClient.close();
-		redisPublisher.quit();
+		dbClient.close()
+		redisPublisher.quit()
 	})
 
 
